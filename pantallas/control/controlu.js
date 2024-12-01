dca3ff3c2dc6d05ceea90920e73344705ced18c2
Deltajoestar04 <jpreciado808@gmail.com>
2024-12-01 09:58:56 -0600

    d

--- pantallas/control/controlu.js
+++ pantallas/control/controlu.js
@@ -1,9 +1,9 @@
 export const controlu = {
   listausers: [
     { username: "Juan Pérez", telefono: "1234567890", puesto: "Administrador" },
-    { username: "Ana Gómez", telefono: "0987654321", puesto: "Usuario" },
-    { username: "Leon Kenedi", telefono: "543345534", puesto: "Usuario" },
-    { username: "Chinga Esperma", telefono: "768876876", puesto: "Usuario" },
+    { username: "Ana Gómez", telefono: "0987654321", puesto: "Trabajador" },
+    { username: "Leon Kenedi", telefono: "543345534", puesto: "Trabajador" },
+    { username: "Chinga Esperma", telefono: "768876876", puesto: "Trabajador" },
   ],
   agregar: function (username, password, telefono, puesto) {
     this.listausers.push({ username, telefono, puesto });
